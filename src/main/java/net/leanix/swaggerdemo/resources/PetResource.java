c85b3685d167eb2cf205cd09ad66f2198feeed9a
Daniel Pozzi <daniel.pozzi@leanix.net>
2015-08-20 21:56:16 +0200

    added DELETE

--- src/main/java/net/leanix/swaggerdemo/resources/PetResource.java
+++ src/main/java/net/leanix/swaggerdemo/resources/PetResource.java
@@ -8,13 +8,16 @@ import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.inject.Inject;
 import javax.validation.Valid;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import net.leanix.swaggerdemo.swagger.AppSwaggerSpecFilter;
 import net.leanix.swaggerdemo.core.Animal;
 import net.leanix.swaggerdemo.core.Shelter;
@@ -59,6 +62,7 @@ public class PetResource
     @GET
     @Path("/{name}")
     @ApiOperation(value = "byName", notes = "Get a pet by its name", response = Animal.class)
+    @ApiResponses(value = {@ApiResponse(code = 404, message = "Not found")})
     public Animal byName(@PathParam("name") String name)
     {
         return shelter.find(name);
@@ -71,5 +75,22 @@ public class PetResource
     {
         shelter.add(pet);
         return pet;
+    }
+    
+    @DELETE
+    @Path("/{name}")
+    @ApiOperation(value = "remove", notes="Remove a pet from the shelter", response = Animal.class)
+    @ApiResponses(value = {@ApiResponse(code = 404, message = "Not found")})
+    public Animal remove(@PathParam("name") String name)
+    {
+        Animal animal = shelter.find(name);
+        if (animal == null)
+            throw new WebApplicationException(
+                    "An animal with the given name does not exist.",
+                    Response.Status.NOT_FOUND
+            );
+        
+        shelter.remove(name);
+        return animal;
     }
 }
