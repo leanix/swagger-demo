758feefe057689c92e1e7e893aeaf496ff98d9cf
Daniel Pozzi <daniel.pozzi@leanix.net>
2015-08-20 21:46:11 +0200

    added /pets POST

--- src/main/java/net/leanix/swaggerdemo/resources/PetResource.java
+++ src/main/java/net/leanix/swaggerdemo/resources/PetResource.java
@@ -3,9 +3,13 @@ package net.leanix.swaggerdemo.resources;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,7 +46,8 @@ public class PetResource
      */
     @GET
     @ApiOperation(
-            value = "Lists all the animals in the shelter.", 
+            value = "getAll",
+            notes = "Lists all the animals in the shelter.", 
             responseContainer = "List", 
             response = Animal.class
     )
@@ -53,9 +58,18 @@ public class PetResource
     
     @GET
     @Path("/{name}")
-    @ApiOperation(value = "Get a pet by its name", response = Animal.class)
+    @ApiOperation(value = "byName", notes = "Get a pet by its name", response = Animal.class)
     public Animal byName(@PathParam("name") String name)
     {
         return shelter.find(name);
+    }
+    
+    @POST
+    @ApiOperation(value = "add", notes="Add a pet to the shelter", response = Animal.class)
+    @ApiResponses(value = {@ApiResponse(code = 422, message = "Invalid entity")})
+    public Animal add(@Valid Animal pet)
+    {
+        shelter.add(pet);
+        return pet;
     }
 }
